c7a1d075ae35ba120ad1c2807df0ad632c7396e8
Tanner Jr., David Arthur <dtannerjr@bwh.harvard.edu>
2025-08-16 16:38:15 -0400

    feat: integrate Chessground for better DnD board

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,7 @@
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 import "../css/app.css";
+import "../css/chessground.css";
 
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
